f64a48e8f1d7f3152fc570172458a418caa5595d
Gabriela Almeida <gabrielabrazolin@outlook.com>
2024-01-22 15:35:01 -0300

    incluindo teste da logica do leitor de arquivo

--- Alura.Adopet.Console/Util/LeitorDeArquivo.cs
+++ Alura.Adopet.Console/Util/LeitorDeArquivo.cs
@@ -18,13 +18,7 @@ namespace Alura.Adopet.Console.Util
                 System.Console.WriteLine("----- Serão importados os dados abaixo -----");
                 while (!sr.EndOfStream)
                 {
-                    // separa linha usando ponto e vírgula
-                    string[] propriedades = sr.ReadLine().Split(';');
-                    // cria objeto Pet a partir da separação
-                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                    propriedades[1],
-                    int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
-                    );
+                    Pet pet = sr.ReadLine().ConverteDoTexto();
 
                     listaDePet.Add(pet);
                 }
